d01e9cfff109f2516771bf016738d2ad4d5e005a
Shravani Arun Panaskar <92431610+S-hravani@users.noreply.github.com>
2022-04-17 19:20:46 +0530

    Update Program to find whether inputted number Divisible by 5 and 7 or not.c

--- Basic Programs/Program to find whether inputted number Divisible by 5 and 7 or not.c
+++ Basic Programs/Program to find whether inputted number Divisible by 5 and 7 or not.c
@@ -9,7 +9,7 @@ int main()
 	printf("\n Enter the Number = ");
 	scanf("%d",&Num);
 	
-    if(Num % 5 == 0 && Num % 7 == 0)
+        if(Num % 5 == 0 && Num % 7 == 0)
 	{
 		printf("\n Given number %d is divisible by 5 and 7 both",Num);
 	}	
